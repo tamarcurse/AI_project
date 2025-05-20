d8c023f3a597f27c27a64e8cf903619672bc95aa
Tamar gold <tamarl3251@gmail.com>
2025-05-20 12:09:12 +0300

    update searcher class

--- src/searcher/sercher.py
+++ src/searcher/sercher.py
@@ -1,6 +1,11 @@
+from model import CModel
+from data_mgr.Data_mgr import CDataMgr
 class CSreachar:
     def __init__(self):
-        self._data = ""
-    def updat_data(self, data):
-        self._data = data
+        self._data = CDataMgr()
+        self._model = CModel()
+    def answer(self, question:str) ->str:
+        prompt = f"Given the following data:\n{self._data.get_data()}\nAnswer the question: {question}"
+        response = self._model.get_response(prompt)
+        return response
     
